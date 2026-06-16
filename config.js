d4db75d574de86dcedbb8d7d2a5767e526147e10
copilotmediagroup <copilotmediagroup@gmail.com>
2026-06-16 12:34:25 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// Co Pilot Collections Manager - Public Supabase Config
+// This file is safe to upload to GitHub ONLY with the publishable key.
+// Do NOT put your sb_secret_ key here.
+// Employees will not see this on the login screen.
+
+window.CO_PILOT_SUPABASE_CONFIG = {
+  url: "https://bwvufgzbkaymffwxuuzr.supabase.co",
+  publishableKey: "PASTE_YOUR_SB_PUBLISHABLE_KEY_HERE"
+};
