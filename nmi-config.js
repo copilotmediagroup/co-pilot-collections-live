e407d267c0061580830562525be01d2807bbd400
copilotmediagroup <copilotmediagroup@gmail.com>
2026-07-20 16:12:22 -0400

    Update nmi-config.js

--- nmi-config.js
+++ nmi-config.js
@@ -4,6 +4,6 @@
 window.CO_PILOT_NMI_CONFIG = {
   enabled: true,
   mode: "production",
-  publicTokenizationKey: "PASTE_NMI_PUBLIC_TOKENIZATION_KEY_HERE",
+  publicTokenizationKey: "GX4Vme-2Q6f2Z-EFe578-33S4j8",
   edgeFunctionName: "nmi-payments"
 };
